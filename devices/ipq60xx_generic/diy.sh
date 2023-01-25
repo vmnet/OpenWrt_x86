0904dfe79a0244477b20e199fc4e85173e48ba69
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2023-01-25 20:50:46 +0800

    update

--- devices/ipq60xx_generic/diy.sh
+++ devices/ipq60xx_generic/diy.sh
@@ -2,7 +2,7 @@
 shopt -s extglob
 
 rm -rf package/system/opkg && mv -f feeds/kiddin9/opkg package/system/
-rm -rf feeds/kiddin9/{rtl*,nft-fullcone,shortcut-fe,fullconenat} package/kernel/mt76 toolchain/musl package/feeds/packages/{jool,xtables-addons,openvswitch}
+rm -rf package/libs feeds/kiddin9/{rtl*,nft-fullcone,shortcut-fe,fullconenat} package/kernel/mt76 toolchain/musl package/feeds/packages/{jool,xtables-addons,openvswitch}
 
 rm -rf target/imagebuilder
 svn co https://github.com/openwrt/openwrt/branches/openwrt-22.03/target/imagebuilder target/imagebuilder
@@ -11,6 +11,8 @@ sed -i "/libfakeroot/d" target/imagebuilder/Makefile
 svn co https://github.com/openwrt/openwrt/branches/openwrt-22.03/toolchain/musl toolchain/musl
 
 svn co https://github.com/coolsnowwolf/openwrt-gl-ax1800/trunk/package/network/services/fullconenat feeds/kiddin9/fullconenat
+
+svn co https://github.com/openwrt/openwrt/branches/openwrt-22.03/package/libs package/libs
 
 rm -rf devices/common/patches/{targets.patch,kernel-defaults.patch,fix.patch,iptables.patch,disable_flock.patch}
 
